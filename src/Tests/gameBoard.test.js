ae13a0addb742d67af64d64fdbf671dc20f90c37
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-07-25 17:29:51 -0400

    Add game board component test suite

---
+++ src/Tests/gameBoard.test.js
@@ -0,0 +1,5 @@
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import GameBoard from '../Components/gameBoard';
+
+configure({ adapter: new Adapter() });
