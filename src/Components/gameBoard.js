9bce91cdc6e7aac916d404e2990e81807e020009
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-07-25 17:42:45 -0400

    Adding table to game board

--- src/Components/gameBoard.js
+++ src/Components/gameBoard.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class GameBoard extends Component {
     render() {
         return (
-            <div className="GameBoard"></div>
+            <div className="GameBoard">
+                <table></table>
+            </div>
         );
     }
 }
