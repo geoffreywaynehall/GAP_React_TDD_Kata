2eaeb828e736a3433220c79419e060e04a37f522
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-07-25 16:46:18 -0400

    App title text test

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,7 @@ configure({ adapter: new Adapter() });
 test('displays the title of the app', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('.title').exists()).toBe(true);
+    expect(wrapper.find('.title').text()).toBe("Tic-Tac-Toe");
 });
 
 test('renders the game board component', () => {
