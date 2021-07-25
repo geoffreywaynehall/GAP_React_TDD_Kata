49e9ff2a3011e3ce66a87131e63fa8f984631e16
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-07-25 16:14:02 -0400

    Game Board Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+configure({ adapter: new Adapter() });
+
+test('renders the game board', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('#game-board').exists()).toBeTruthy();
+});
