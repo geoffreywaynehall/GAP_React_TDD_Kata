9117b95af00b778bc1477ea75d03f56022786934
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-07-25 17:54:14 -0400

    Test table cell count

--- src/Tests/gameBoard.test.js
+++ src/Tests/gameBoard.test.js
@@ -7,6 +7,8 @@ configure({ adapter: new Adapter() });
 describe('on start', () => {
     test('displays a blank 3x3 table', () => {
         const wrapper = shallow(<GameBoard />);
-        expect(wrapper.find('table').exists()).toBe(true);
+        const table = wrapper.find('table');
+        expect(table.exists()).toBe(true);
+        expect(table.find('td')).toHaveLength(9);
     });
 });
