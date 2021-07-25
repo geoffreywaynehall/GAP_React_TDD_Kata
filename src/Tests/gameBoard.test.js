db49f67ecc40099644e422004aa105c8af3c907f
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-07-25 17:37:18 -0400

    blank game board on start test

--- src/Tests/gameBoard.test.js
+++ src/Tests/gameBoard.test.js
@@ -3,3 +3,10 @@ import Adapter from 'enzyme-adapter-react-16';
 import GameBoard from '../Components/gameBoard';
 
 configure({ adapter: new Adapter() });
+
+describe('on start', () => {
+    test('displays a blank 3x3 table', () => {
+        const wrapper = shallow(<GameBoard />);
+        expect(wrapper.find('table').exists()).toBe(true);
+    });
+});
