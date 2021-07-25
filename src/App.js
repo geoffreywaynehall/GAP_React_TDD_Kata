8b639fdc854771e5fb4b30a01d343ef2bb376173
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-07-25 16:45:09 -0400

    Adding app title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import GameBoard from './Components/gameBoard'
 function App() {
   return (
     <div className="App">
-          <GameBoard />
+        <h1 className='title'></h1>
+        <GameBoard />
     </div>
   );
 }
