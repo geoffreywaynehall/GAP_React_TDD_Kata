48c2598e10532061d01c89de385fdceec663cf33
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-07-25 17:54:47 -0400

    Add cells to table

--- src/Components/gameBoard.js
+++ src/Components/gameBoard.js
@@ -4,7 +4,23 @@ class GameBoard extends Component {
     render() {
         return (
             <div className="GameBoard">
-                <table></table>
+                <table>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
             </div>
         );
     }
