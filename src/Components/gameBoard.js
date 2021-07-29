88cdb8c8e16386db509cfc773bcf2985ac2e6543
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-07-28 22:36:06 -0400

    First move functionality

--- src/Components/gameBoard.js
+++ src/Components/gameBoard.js
@@ -2,25 +2,34 @@ import '../Style/gameBoard.css';
 import React, { Component } from "react";
 
 class GameBoard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            tile: ''
+        };
+    }
+
     render() {
         return (
             <div className="GameBoard">
                 <table>
-                    <tr>
-                        <td></td>
-                        <td className="vertical"></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td className="horizontal"></td>
-                        <td className="horizontal vertical"></td>
-                        <td className="horizontal"></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td className="vertical"></td>
-                        <td></td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td id="square-0" onClick={() => this.setState({ tile: 'X' })}>{ this.state.tile }</td>
+                            <td className="vertical"></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td className="horizontal"></td>
+                            <td className="horizontal vertical"></td>
+                            <td className="horizontal"></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td className="vertical"></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
                 </table>
                 <h2 className="turn-tracker">It is X's turn to go!</h2>
             </div>
