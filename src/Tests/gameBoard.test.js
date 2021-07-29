7ac215fcd9de1e030f2199998ab5623fee2b49b9
Geoffrey Hall <geoffreywaynehall@gmail.com>
2021-07-28 23:26:26 -0400

    test cleanup

--- src/Tests/gameBoard.test.js
+++ src/Tests/gameBoard.test.js
@@ -4,9 +4,14 @@ import GameBoard from '../Components/gameBoard';
 
 configure({ adapter: new Adapter() });
 
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallow(<GameBoard />);
+});
+
 describe('on start', () => {
     test('displays a blank 3x3 table', () => {
-        const wrapper = shallow(<GameBoard />);
         const table = wrapper.find('table');
         const cells = table.find('td');
         expect(table.exists()).toBe(true);
@@ -17,36 +22,36 @@ describe('on start', () => {
     });
 
     test('displays whose turn it is to go first', () => {
-        const wrapper = shallow(<GameBoard />);
-        expect(wrapper.find('.turn-tracker').exists()).toBe(true);
-        expect(wrapper.find('.turn-tracker').text()).toBe("It is X's turn to go!");
+        const turnTracker = wrapper.find('.turn-tracker');
+        expect(turnTracker.exists()).toBe(true);
+        expect(turnTracker.text()).toBe("It is X's turn to go!");
     });
 });
 
 describe('during gameplay', () => {
     test('the first player to click a square will cause an "X" to appear on that square', () => {
-        const wrapper = shallow(<GameBoard />);
-        wrapper.find('#square-0').simulate('click');
-        expect(wrapper.find('#square-0').text()).toBe('X');
+        const firstSquare = wrapper.find('#square-0');
+        firstSquare.simulate('click');
+        expect(firstSquare.text()).toBe('X');
     });
 
     test('the turn tracker is updated', () => {
-        const wrapper = shallow(<GameBoard />);
         wrapper.find('#square-0').simulate('click');
         expect(wrapper.find('.turn-tracker').text()).toBe("It is O's turn to go!");
     });
 
     test('the second player to click a different square will cause an "O" to appear on that square', () => {
-        const wrapper = shallow(<GameBoard />);
         wrapper.find('#square-0').simulate('click');
-        wrapper.find('#square-1').simulate('click');
-        expect(wrapper.find('#square-1').text()).toBe('O');
+        const secondSquare = wrapper.find('#square-1');
+        secondSquare.simulate('click');
+        expect(secondSquare.text()).toBe('O');
     });
 
     test('clicking the same square twice will not count as a turn', () => {
-        const wrapper = shallow(<GameBoard />);
-        wrapper.find('#square-0').simulate('click');
-        wrapper.find('#square-0').simulate('click');
-        expect(wrapper.find('#square-0').text()).toBe('X');
+        const firstSquare = wrapper.find('#square-0');
+        firstSquare.simulate('click');
+        firstSquare.simulate('click');
+        expect(firstSquare.text()).toBe('X');
+        expect(wrapper.find('.turn-tracker').text()).toBe("It is O's turn to go!");
     });
 });
